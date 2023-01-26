5635fbd9957e0c78c418f8257fd8150400627279
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 16:49:21 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+string str = Convert.ToString(number);
+int len = str.Length;
+int count = 0;
+
+if(len < 3)
+{
+    WriteLine("Третьей цифры нет");
+}
+
+while(count < len-3)
+{
+    number = number/10;
+    count += 1;
+}
+
+WriteLine(number%10);
