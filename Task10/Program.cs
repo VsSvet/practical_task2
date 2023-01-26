eb9b3e2d707384cd576f55d8cf40c489e2115986
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 15:30:59 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int number1 = number%100;
+
+Console.Write($"Вторая цифра числа {number} равна {number1/10}");
