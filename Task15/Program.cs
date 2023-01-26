31e20e3106e3ec571d20070c1c070d6b6301cce3
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 17:08:09 +0300

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+
+if(number > 0 & number < 6)
+{
+    WriteLine($"{number} -> нет");
+}
+
+if(number > 5 & number < 8)
+{
+    WriteLine($"{number} -> да");
+}
+
+if(number< 1 || number > 7)
+{
+    WriteLine($"Неправильный номер дня недели");
+}
